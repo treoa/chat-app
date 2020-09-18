677e3b241adef63fded9ad716971c258c66f6351
treoa <anuar.suleimenov@nu.edu.kz>
2020-09-19 04:09:26 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Chat Web App</h1>
-      </header>
+      <Header />
+      {/* {The card} */}
+      {/* {Footer} */}
     </div>
   );
 }
