da7b427feecf30a3799f525452c4b8e612e228b3
treoa <anuar.suleimenov@nu.edu.kz>
2020-09-19 06:50:53 +0600

    finished

--- src/backend/dbCards.js
+++ src/backend/dbCards.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const cardSchema = mongoose.Schema({
     name: String,
-    imgUrl: String,
+    url: String,
 })
 
 export default mongoose.model('cards', cardSchema)
